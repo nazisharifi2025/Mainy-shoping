5e18d05f89439ffb696026f207c86da0193930c0
nazi sharifi <banonazisharifi@gmail.com>
2026-01-17 19:46:21 +0430

    added Home page

--- app/Http/Controllers/CastumarController.php
+++ app/Http/Controllers/CastumarController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CastumarController extends Controller
 {
-    //
+    public function index(){
+        $users = User::all();
+        return view('Customer.Home' , ['users' , $users]);
+    }
 }
---
+++ resources/views/Customer/Home.blade.php
@@ -0,0 +1,24 @@
+<html>
+    <heade></heade>
+    <body class="h-screen w-full overflow-y-scroll">
+        <div class="w-11/12 mx-auto flex flex-col justify-center items-center">
+            <h1 class="text-4xl font-bold text-center">All Customer</h1>
+            <table class="border border-collapse w-full">
+                <tr>
+                    <th class="py-2 px-4 border">Id</th>
+                    <th class="py-2 px-4 border">Name</th>
+                    <th class="py-2 px-4 border">LastName</th>
+                    <th class="py-2 px-4 border">Email</th>
+                </tr>
+                @foreach ($users as $user)
+                     <tr>
+                        <td class="border px-4 py-1">{{ $user->id }}</td>
+                        <td class="border px-4 py-1">{{ $user->name }}</td>
+                        <td class="border px-4 py-1">{{ $user->lastName }}</td>
+                        <td class="border px-4 py-1">{{ $user->email }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    </body>
+</html>
