b9626d8760391b02138436672320c7d65a8ea01b
nazi sharifi <banonazisharifi@gmail.com>
2026-01-31 23:25:10 +0430

    added new chenges

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function cart(Request $request, $id,$price){
+        $product = Product::findOrFail($id);
+        $cart = session()->get('cart' , []);
+        if(isset($cart[$id])){
+            $cart[$id]['quantity']++;
+        }else{
+            $cart[$id] = [
+            'id'=> $product->id,
+            'name'=> $product->name,
+            'price'=> $price,
+            'quantity' => 1
+            ];
+        }
+    }
 }
